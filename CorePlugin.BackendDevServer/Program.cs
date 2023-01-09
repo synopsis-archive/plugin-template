78257bb3d91bab76fc3d5d8d2e19c8adffef10dc
Andreas Aigner <aigner.andi05@gmail.com>
2023-01-09 10:23:10 +0100

    Add cors to BackendDevServer

--- CorePlugin.BackendDevServer/Program.cs
+++ CorePlugin.BackendDevServer/Program.cs
@@ -32,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
