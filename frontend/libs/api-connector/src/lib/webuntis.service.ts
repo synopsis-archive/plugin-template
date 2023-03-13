231455ced852d500d4e46cfce65c9ef499e61942
Fabian <fabian.paul.gurtner@gmail.com>
2023-03-13 09:42:50 +0100

    Implement mainframe service

--- frontend/libs/api-connector/src/lib/webuntis.service.ts
+++ frontend/libs/api-connector/src/lib/webuntis.service.ts
@@ -1,11 +1,10 @@
 import {Injectable} from "@angular/core";
+import {MainframeService} from "./mainframe.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class WebUntisService {
-  constructor() {
+  constructor(private mainframe: MainframeService) {
   }
-
-
 }
